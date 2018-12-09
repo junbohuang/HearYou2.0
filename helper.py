b3ec19e45c9307736f739178ded93ed382609add
junbohuang <junbo.huang.sz@gmail.com>
2018-12-09 17:35:00 +0100

    some new result after months...

--- helper.py
+++ helper.py
@@ -796,7 +796,7 @@ def train(config, model, xtrain, ytrain, xtest, ytest):
         f.write(model.to_json())
 
     # check_pointer = ModelCheckpoint(save_path, save_best_only=True)
-    early_stopping = EarlyStopping(monitor='val_loss', patience=5)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=10)
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
